b63d68d0c12915ebb49504662f57e075736ebaa9
KEEBO\Dharla <dlockwood024548@students.iticollege.edu>
2021-05-03 16:24:03 -0500

    Updated Basic Student Info Form, Add Student Form, and Student Profile Form

---
+++ StudentManagementRD/StudentManagementRD/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StudentManagementRD
+{
+    public class Course
+    {
+        object currentCourses;
+        int term;
+        public object GetCurrentCourses(string studentID)
+        {
+            DbConnection courseConnection = new DbConnection();
+            courseConnection.OpenConnection();
+            currentCourses = courseConnection.ShowDataInGridView("SELECT Grades.CourseID AS Course, CourseName AS Course_Title, Term " +
+                "FROM Grades INNER JOIN Course ON Grades.CourseID = Course.CourseID WHERE Grades.StudentID = " + studentID + " AND Term LIKE '2021-SPR%';");
+            courseConnection.CloseConnection();
+            return currentCourses;
+        }
+        public int GetTerm(string studentID)
+        {
+            DbConnection termConnection = new DbConnection();
+            termConnection.OpenConnection();
+            SqlDataReader dataReader = termConnection.DataReader("SELECT COUNT(Term) AS CurrentTerm FROM Grades WHERE StudentID=" + studentID);
+            dataReader.Read();
+            term = Convert.ToInt32(dataReader["CurrentTerm"]);
+            termConnection.CloseConnection();
+            term = term / 3;
+            return term;
+        }
+    }
+
+}
---
+++ StudentManagementRD/StudentManagementRD/frmStudentProfile.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManagementRD
+{
+    public partial class frmStudentProfile : Form
+    {
+        public frmStudentProfile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFindStudent_Click(object sender, EventArgs e)
+        {
+            string firstName;
+            string lastName;
+            string studentID;
+            string gpa;
+
+            Student detailedStudent = new Student();
+            Course smallCourse = new Course();
+            
+                if (txtStudentID.Text.Length >= 1)
+                {
+                    studentID = txtStudentID.Text;
+                    txtFirstName.Text = detailedStudent.GetFirstName(studentID);
+                    txtLastName.Text = detailedStudent.GetLastName(studentID);
+                }
+                else if (txtFirstName.Text.Length >= 1 && txtLastName.Text.Length >= 1)
+                {
+                    firstName = txtFirstName.Text;
+                    lastName = txtLastName.Text;
+                    txtStudentID.Text = detailedStudent.GetStudentID(firstName, lastName);
+                }
+                else
+                {
+                    MessageBox.Show("Error finding student. \n\nPlease make sure you have either the student's first name and last named entered correctly, or their student ID number entered correctly.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+            studentID = txtStudentID.Text;
+            txtPhoneNumber.Text = detailedStudent.GetPhoneNumber(studentID);
+            txtEmail.Text = detailedStudent.GetEmail(studentID);
+            dtpDateOfBirth.Value = Convert.ToDateTime(detailedStudent.GetDateOfBirth(studentID));
+            txtAddress.Text = detailedStudent.GetAddress(studentID);
+            txtCity.Text = detailedStudent.GetCity(studentID);
+            txtParish.Text = detailedStudent.GetParish(studentID);
+            txtZipCode.Text = detailedStudent.GetZipCode(studentID);
+            dtpEnrollDate.Value = Convert.ToDateTime(detailedStudent.GetEnrollDate(studentID));
+            dtpGraduationDate.Value = Convert.ToDateTime(detailedStudent.GetGradDate(studentID));
+            txtDegree.Text = detailedStudent.GetDegree(studentID);
+            lblDepartment.Text = detailedStudent.GetDepartment(studentID);
+            lblDepartmentHead.Text = detailedStudent.GetDepartmentHead(lblDepartment.Text);
+            lblTerm.Text = smallCourse.GetTerm(studentID).ToString();
+            txtDegree.Text = detailedStudent.GetDegree(studentID);
+            if (lblTerm.Text == "1" || lblTerm.Text == "0")
+            {
+                lblGPA.Text = "N/A";
+            }
+            else
+            {
+
+                lblGPA.Text = detailedStudent.GetGPA(studentID).ToString("0.00");
+            }
+        }
+    }
+}
